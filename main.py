d297d76cabae9b4bf8ee80e0ca2e52df7f88a5e2
Justin-Morman <justinmorman@gmail.com>
2026-04-28 07:08:30 -0400

    initial setup complete

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+from google.genai import types
+
+
+
+
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+user_prompt = args.user_prompt
+verbose = args.verbose
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 
 
@@ -11,8 +26,16 @@ if not api_key:
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=messages
     )
+if verbose is True:
+    if response.usage_metadata is not None:
+        print(f"User prompt: {args.user_prompt}")
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    else:
+        raise RuntimeError("No usage metadata found")
+
 print(response.text)
 
 def main():
